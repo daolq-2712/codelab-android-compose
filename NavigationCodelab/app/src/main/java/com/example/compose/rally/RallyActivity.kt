34e91c6dae9d0450598ec091c1f6191f382410d8
le.quang.dao <le.quang.dao@sun-asterisk.com>
2024-07-30 16:59:52 +0700

    Navigate to another screen with arguments

--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -19,21 +19,22 @@ package com.example.compose.rally
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.compose.rally.navigation.navigateSingleTopTo
+import com.example.compose.rally.navigation.navigateToSingleAccount
+import com.example.compose.rally.ui.accounts.AccountsScreen
+import com.example.compose.rally.ui.accounts.SingleAccountScreen
+import com.example.compose.rally.ui.bills.BillsScreen
 import com.example.compose.rally.ui.components.RallyTabRow
+import com.example.compose.rally.ui.overview.OverviewScreen
 import com.example.compose.rally.ui.theme.RallyTheme
 
 /**
@@ -59,7 +60,8 @@ fun RallyApp() {
         val currentDestination = currentBackStack?.destination
 
         // Change the variable to this and use Overview as a backup screen if this returns null
-        val currentScreen = rallyTabRowScreens.find { it.route == currentDestination?.route } ?: Overview
+        val currentScreen =
+            rallyTabRowScreens.find { it.route == currentDestination?.route } ?: Overview
 
         Scaffold(
             topBar = {
@@ -78,13 +80,34 @@ fun RallyApp() {
                 Modifier.padding(innerPadding)
             ) {
                 composable(route = Overview.route) {
-                    Overview.screen()
+                    OverviewScreen(onClickSeeAllAccounts = {
+                        navController.navigateSingleTopTo(
+                            Accounts.route
+                        )
+                    }, onClickSeeAllBills = {
+                        navController.navigateSingleTopTo(Bills.route)
+                    }, onAccountClick = { accountType ->
+                        navController.navigateToSingleAccount(accountType)
+                    })
                 }
                 composable(route = Accounts.route) {
-                    Accounts.screen()
+                    AccountsScreen(onAccountClick = { accountType ->
+                        navController.navigateToSingleAccount(accountType)
+                    })
                 }
                 composable(route = Bills.route) {
-                    Bills.screen()
+                    BillsScreen()
+                }
+                composable(
+                    route = SingleAccount.routeWithArgs,
+                    arguments = SingleAccount.arguments
+                )
+                { navBackStackEntry ->
+                    // Retrieve the passed argument
+                    val accountType =
+                        navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
+                    // Pass accountType to SingleAccountScreen
+                    SingleAccountScreen(accountType)
                 }
             }
         }
--- NavigationCodelab/app/src/main/java/com/example/compose/rally/navigation/NavHostControllerExt.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/navigation/NavHostControllerExt.kt
@@ -2,6 +2,7 @@ package com.example.compose.rally.navigation
 
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import com.example.compose.rally.SingleAccount
 
 fun NavHostController.navigateSingleTopTo(route: String) =
     this.navigate(route) {
@@ -11,3 +12,7 @@ fun NavHostController.navigateSingleTopTo(route: String) =
         launchSingleTop = true
         restoreState = true
     }
+
+fun NavHostController.navigateToSingleAccount(accountType: String) {
+    this.navigateSingleTopTo("${SingleAccount.route}/$accountType")
+}
